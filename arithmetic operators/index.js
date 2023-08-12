9fbb2bbff3e2e4964ec5d9c37a49f563bd0fa306
chethanavirajini <cheth.virajini@gmail.com>
2023-08-12 23:07:15 +0530

    addition

---
+++ arithmetic operators/index.js
@@ -0,0 +1,12 @@
+console.log("run page")
+
+var a = 12;
+var b = 10;
+var c = "text";
+var line = "<br/>"
+
+// addition
+document.write("Addition" + line);
+document.write("A + B = ");
+result = a = b;
+document.write(result)
