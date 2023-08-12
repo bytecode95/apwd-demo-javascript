56bfbac40491574351e69b63f78e71b251f713b4
chethanavirajini <cheth.virajini@gmail.com>
2023-08-12 23:32:28 +0530

    Comparision operation

---
+++ arithmetic operators/index2.js
@@ -0,0 +1,46 @@
+console.log("run page")
+var a = 12;
+var b =10;
+var line = "<br/>"
+
+// ==(Equal)
+document.write("Equal"+line);
+document.write("a == b: ");
+result = a == b;
+document.write(result);
+document.write(line, line)
+
+// ==(Greater than)
+document.write("Greaterthan"+line);
+document.write("a > b: ");
+result = a > b;
+document.write(result);
+document.write(line, line)
+
+// ==(Less than)
+document.write("Lessthan"+line);
+document.write("a < b: ");
+result = a < b;
+document.write(result);
+document.write(line, line)
+
+// ==(Not Equal)
+document.write("NotEqual"+line);
+document.write("a != b: ");
+result = a != b;
+document.write(result);
+document.write(line, line);
+
+// ==(Grater than or Equal)
+document.write("Greaterthan or equal to"+line);
+document.write("a >= b: ");
+result = a >= b;
+document.write(result);
+document.write(line, line)
+
+// ==(Less than or Equal)
+document.write("Lessthan or equal to"+line);
+document.write("a <= b: ");
+result = a <= b;
+document.write(result);
+document.write(line, line)
