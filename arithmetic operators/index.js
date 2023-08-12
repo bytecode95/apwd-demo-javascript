2bb744bc5f3dfd40f67b0d28bb2bab14b7ba8f3b
chethanavirajini <cheth.virajini@gmail.com>
2023-08-12 23:19:09 +0530

    arithemetic operations

--- arithmetic operators/index.js
+++ arithmetic operators/index.js
@@ -8,5 +8,54 @@ var line = "<br/>"
 // addition
 document.write("Addition" + line);
 document.write("A + B = ");
-result = a = b;
-document.write(result)
+result = a + b;
+document.write(result);
+document.write(line,line);
+
+//Substraction
+
+document.write("Substraction" + line);
+document.write("A - B = ");
+result = a - b;
+document.write(result);
+document.write(line,line);
+
+//Multiplication
+
+document.write("Multiplication" + line);
+document.write("A * B = ");
+result = a * b;
+document.write(result);
+document.write(line,line);
+
+//Division
+
+document.write("Division" + line);
+document.write("A / B = ");
+result = a / b;
+document.write(result);
+document.write(line,line);
+
+//Modulus
+
+document.write("Modulus" + line);
+document.write("A % B = ");
+result = a % b;
+document.write(result);
+document.write(line,line);
+
+//Increment
+
+document.write("Increment" + line);
+document.write("++ b = ");
+result = ++b;
+document.write(result);
+document.write(line,line);
+
+//Decrement
+
+document.write("Decrement" + line);
+document.write("-- a = ");
+result = --a;
+document.write(result);
+document.write(line,line);
