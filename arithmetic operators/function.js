bda8b5cc42322c46b31cdf234c1605baa9ffbcd6
chethanavirajini <cheth.virajini@gmail.com>
2023-08-13 13:10:56 +0530

    Flow controllers

--- arithmetic operators/function.js
+++ arithmetic operators/function.js
@@ -55,13 +55,82 @@ function maths2(one, two){
     console.log(total)
 }
 
-
 //return function
-
 //if return value is not defined undefind value will be come when function is calling
 function maths2023(one, two){
     let total = one * two;
     //console.log(total)
     return total
 }
+console.log("Flow controllers..................")
 
+//Flow controllers
+// if - when if condition is true , argument will be implemeted,  otherwise else will be implmented
+if(false){
+    alert("Hello");
+}
+
+
+const marks = 73;
+mathsTotal = 90;
+if(marks < 30){
+    //console.log("Exam failed")
+}
+
+console.log("if else..................")
+
+// if else
+
+if(marks > 50){
+    console.log("Exam passed")
+}else{
+    console.log("Exam failed")
+}
+
+// else if 
+console.log("else if..................")
+
+if(marks >= 75){
+    console.log("Grade : A")
+}else if(marks >= 65){
+    console.log("Grade: B")
+}else if(marks >= 40){
+    console.log("Grade: C")
+}else{
+    console.log("Grade :  fail")
+}
+
+
+console.log("nested if..................")
+//nested if
+
+if(marks >= 75){
+    console.log("Exam passed")
+    if(mathsTotal > 50){
+        console.log("Maths pass...")
+    }else{
+        console.log("Maths Fail...")
+    }
+}else{
+    // if(mathsTotal < 50){
+    //     console.log("Maths fail...")
+    // }
+}
+
+console.log("Switch..................")
+//Switch - fix values should be used
+
+switch(marks){
+    case 75,73: 
+        console.log("Grade : A");
+        break;
+    case 65:
+        console.log("Grade : B");
+        break; 
+    case 55:
+        console.log("Grade : C");
+        break;   
+    default:
+        console.log("Fail")    
+
+}
