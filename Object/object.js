9e24f718002f3884c470595b8ff7783e5633e998
chethanavirajini <cheth.virajini@gmail.com>
2023-08-17 15:14:23 +0530

    object

---
+++ Object/object.js
@@ -0,0 +1,13 @@
+console.log('object page');
+
+let person  = {
+    firstName: "Chethana",
+    lastName: "Virajini",
+    eyeColor: "black",
+    run: function(){
+        alert('run');
+    }
+
+}
+console.log(person.firstName +" "+ person.lastName);
+person.run();
