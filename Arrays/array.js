27c625763595dd4b941e344f7f8f9158f92b822f
chethanavirajini <cheth.virajini@gmail.com>
2023-08-17 13:58:19 +0530

    js array

---
+++ Arrays/array.js
@@ -0,0 +1,24 @@
+console.log("array page");
+
+const name = "Kamal";
+const name1 = "Sunil";
+const name2 = "Jayantha";
+
+const names = [true , name1, 55, {age:23, skin:"soft"}, [{},{}]];
+const d = [];
+const values = [2,6,4,8]
+console.log(typeof(names));
+console.log(typeof(values));
+console.log(typeof(d));
+
+console.log(names);
+console.log(names[2]);
+console.log(names[4]);
+
+const cars = [];
+cars[0] = "BMW"
+cars[1] = "Toyota"
+cars[2] = "Volvo"
+cars[3] = "Ford"
+console.log(cars)
+
