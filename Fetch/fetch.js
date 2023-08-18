33f0c0ed6f42f837a8e7c62269a4a59b02c751de
chethanavirajini <cheth.virajini@gmail.com>
2023-08-18 23:48:07 +0530

    fetch method

--- Fetch/fetch.js
+++ Fetch/fetch.js
@@ -4,4 +4,24 @@ function loadScript(){
     fetch('https://jsonplaceholder.typicode.com/todos')
         .then(response => response.json())
         .then(json => console.log(json))
+        .catch(err=>console.log(err));
+
+    $("#btn-click").click(function(){
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify({
+                title: 'foo',
+                body: 'bar',
+                userId: 1,
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((response) => response.json())
+            .then((json) => console.log(json));
+    })
+
+
+
 }
