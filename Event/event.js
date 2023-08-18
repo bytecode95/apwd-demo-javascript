acd15c2af3ce222d44fbb2cf1898ebcab414c80e
chethanavirajini <cheth.virajini@gmail.com>
2023-08-18 11:52:07 +0530

    button click event

--- Event/event.js
+++ Event/event.js
@@ -1,44 +1,55 @@
 console.log("event page");
 
-function clickMe(){
+function clickMe() {
     console.log("Clicked");
 }
 
-function mouseOver(){
+function mouseOver() {
     console.log("Mouse Over");
 }
 
-function mouseOut(){
+function mouseOut() {
     console.log("Mouse Out");
 }
 
-function load(){
+function load() {
     console.log("load image");
 }
 
-function onChange(){
+function onChange() {
     console.log("Print type");
 }
-function onKeypress(){
+
+function onKeypress() {
     console.log("Key press");
 }
 
-function onkeyUp(){
+function onkeyUp() {
     console.log("Key up");
 }
 
-function onkeyDown(){
+function onkeyDown() {
     console.log("Key down");
 }
 
 //========================================================================
 
-function submit(){
+function submit() {
     const text = document.getElementById("input-2").value;
     console.log(text);
 }
 
-function onChangeText(){
+function onChangeText() {
     const text1 = document.getElementById("input-2").value;
     console.log(text1);
+}
+
+function onButtonChange(){
+    document.getElementById("btn-1").style.width = '145px';
+    document.getElementById("btn-1").style.backgroundColor = '#990808';
+}
+
+function onButtonChangeout(){
+    document.getElementById("btn-1").style.width = '150px';
+    document.getElementById("btn-1").style.backgroundColor = 'red';
 }
