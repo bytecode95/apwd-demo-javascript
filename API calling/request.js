7280033e168f0238fcfbb2ac5d073136937648cb
chethanavirajini <cheth.virajini@gmail.com>
2023-08-21 15:23:05 +0530

    API - XML & Fetch

---
+++ API calling/request.js
@@ -0,0 +1,24 @@
+console.log(".........XML HTTP request..........");
+
+const request1 = new XMLHttpRequest();
+request1.open("GET","https://jsonplaceholder.typicode.com/users");
+request1.send();
+request1.onload = ()=>{
+    console.log(request1);
+    if(request1.status === 200){
+        console.log(JSON.parse(request1.response));
+    }else{
+        console.log(`error ${request1.status}`);
+    }
+}
+
+console.log(".........Fetch API..........");
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response=>{
+      return response.json();
+}).then(json=>{
+    console.log(json);
+})
+
+console.log(window);
---
+++ High Order Array and functions/practice.js
@@ -0,0 +1,26 @@
+console.log("Practice");
+
+const users = [
+    {firstName: 'John', lastName: 'Doe', age: 25},
+    {firstName: 'Jane', lastName: 'Doe', age: 30},
+    {firstName: 'Jack', lastName: 'Peter', age: 35},
+    {firstName: 'Jill', lastName: 'Johnson', age: 40},
+    {firstName: 'Joe', lastName: 'Wan', age: 45},
+    {firstName: 'Smith', lastName: 'black', age: 45},
+    {firstName: 'Steve', lastName: 'William', age: 45},
+    {firstName: 'Brad', lastName: 'Peter', age: 45},
+    {firstName: 'John', lastName: 'Cena', age: 45},
+    {firstName: 'Kate', lastName: 'Silva', age: 45},
+]
+
+const shoppingCart = [
+    {name: 'Apple', price: 1.99, quantity: 3},
+    {name: 'Apple', price: 1.99, quantity: 3},
+    {name: 'Xiomi', price: 2.99, quantity: 2},
+    {name: 'Samsung', price: 3.99, quantity: 1},
+    {name: 'Tesla', price: 3.99, quantity: 1},
+    {name: 'Tesla', price: 4.99, quantity: 4},
+    {name: 'Nokia', price: 4.99, quantity: 4},
+]
+
+const numbers = [10, 29, 11, 43, 37, 45, 87, 32, 23, 46, 38];
